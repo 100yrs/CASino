4a32be19fb68543d0a362be1e1617416525a3f27
Nick Coyne <nick@codevader.com>
2019-05-02 09:43:14 +1200

    Simplify migrations

--- db/migrate/20130809135400_create_casino_core_schema.rb
+++ db/migrate/20130809135400_create_casino_core_schema.rb
@@ -1,7 +1,7 @@
 # In order to support pre-2.0 installations of CASino that included CASinoCore,
 # we must rebuild the un-namespaced CASinoCore schema so that we can upgrade
-class CreateCoreSchema < ActiveRecord::Migration[4.2]
-  CoreTables = %w{login_tickets proxy_granting_tickets proxy_tickets service_rules service_tickets ticket_granting_tickets two_factor_authenticators users}
+class CreateCasinoCoreSchema < ActiveRecord::Migration[4.2]
+  CoreTables = %w[login_tickets proxy_granting_tickets proxy_tickets service_rules service_tickets ticket_granting_tickets two_factor_authenticators users]
 
   def up
     CoreTables.each do |table_name|
@@ -17,16 +17,16 @@ class CreateCoreSchema < ActiveRecord::Migration[4.2]
   end
 
   def create_login_tickets
-    create_table :login_tickets do |t|
+    create_table :casino_login_tickets do |t|
       t.string :ticket, :null => false
 
       t.timestamps
     end
-    add_index :login_tickets, :ticket, :unique => true
+    add_index :casino_login_tickets, :ticket, :unique => true
   end
 
   def create_proxy_granting_tickets
-    create_table :proxy_granting_tickets do |t|
+    create_table :casino_proxy_granting_tickets do |t|
       t.string  :ticket,       :null => false
       t.string  :iou,          :null => false
       t.integer :granter_id,   :null => false
@@ -35,26 +35,26 @@ class CreateCoreSchema < ActiveRecord::Migration[4.2]
 
       t.timestamps
     end
-    add_index :proxy_granting_tickets, :ticket, :unique => true
-    add_index :proxy_granting_tickets, :iou, :unique => true
-    add_index :proxy_granting_tickets, [:granter_type, :granter_id], :name => "index_proxy_granting_tickets_on_granter", :unique => true
+    add_index :casino_proxy_granting_tickets, :ticket, :unique => true
+    add_index :casino_proxy_granting_tickets, :iou, :unique => true
+    add_index :casino_proxy_granting_tickets, [:granter_type, :granter_id], :name => "index_proxy_granting_tickets_on_granter", :unique => true
   end
 
   def create_proxy_tickets
-    create_table :proxy_tickets do |t|
+    create_table :casino_proxy_tickets do |t|
       t.string  :ticket,                                      :null => false
-      t.string  :service,                                     :null => false
+      t.text  :service,                                     :null => false
       t.boolean :consumed,                 :default => false, :null => false
       t.integer :proxy_granting_ticket_id,                    :null => false
 
       t.timestamps
     end
-    add_index :proxy_tickets, :ticket, :unique => true
-    add_index :proxy_tickets, :proxy_granting_ticket_id
+    add_index :casino_proxy_tickets, :ticket, :unique => true
+    add_index :casino_proxy_tickets, :proxy_granting_ticket_id, name: 'casino_proxy_tickets_on_pgt_id'
   end
 
   def create_service_rules
-    create_table :service_rules do |t|
+    create_table :casino_service_rules do |t|
       t.boolean :enabled, :default => true,  :null => false
       t.integer :order,   :default => 10,    :null => false
       t.string  :name,                       :null => false
@@ -63,55 +63,55 @@ class CreateCoreSchema < ActiveRecord::Migration[4.2]
 
       t.timestamps
     end
-    add_index :service_rules, :url, :unique => true
+    add_index :casino_service_rules, :url, :unique => true
   end
 
   def create_service_tickets
-    create_table :service_tickets do |t|
+    create_table :casino_service_tickets do |t|
       t.string  :ticket,                                      :null => false
-      t.string  :service,                                     :null => false
+      t.text  :service,                                     :null => false
       t.integer :ticket_granting_ticket_id
       t.boolean :consumed,                 :default => false, :null => false
       t.boolean :issued_from_credentials,  :default => false, :null => false
 
       t.timestamps
     end
-    add_index :service_tickets, :ticket, :unique => true
-    add_index :service_tickets, :ticket_granting_ticket_id
+    add_index :casino_service_tickets, :ticket, :unique => true
+    add_index :casino_service_tickets, :ticket_granting_ticket_id, name: 'casino_service_tickets_on_tgt_id'
   end
 
   def create_ticket_granting_tickets
-    create_table :ticket_granting_tickets do |t|
+    create_table :casino_ticket_granting_tickets do |t|
       t.string  :ticket,                                                :null => false
-      t.string  :user_agent
+      t.text  :user_agent
       t.integer :user_id,                                               :null => false
       t.boolean :awaiting_two_factor_authentication, :default => false, :null => false
       t.boolean :long_term,                          :default => false, :null => false
 
       t.timestamps
     end
-    add_index :ticket_granting_tickets, :ticket, :unique => true
+    add_index :casino_ticket_granting_tickets, :ticket, :unique => true
   end
 
   def create_two_factor_authenticators
-    create_table :two_factor_authenticators do |t|
+    create_table :casino_two_factor_authenticators do |t|
       t.integer :user_id,                    :null => false
       t.string  :secret,                     :null => false
       t.boolean :active,  :default => false, :null => false
 
       t.timestamps
     end
-    add_index :two_factor_authenticators, :user_id
+    add_index :casino_two_factor_authenticators, :user_id
   end
 
   def create_users
-    create_table :users do |t|
+    create_table :casino_users do |t|
       t.string  :authenticator,   :null => false
       t.string  :username,        :null => false
       t.text    :extra_attributes
 
       t.timestamps
     end
-    add_index :users, [:authenticator, :username], :unique => true
+    add_index :casino_users, [:authenticator, :username], :unique => true
   end
 end
